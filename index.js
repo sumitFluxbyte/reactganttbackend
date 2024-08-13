befb65906464be0f8c3206820c01d88af318f384
sumitFluxbyte <sumitpatel.fluxbyte@gmail.com>
2024-08-13 11:30:44 +0530

    cors add

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8000
+const cors = require("cors")
 
+app.use(cors("*"))
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/tasks', (req, res) => res.json(
     [
